91b986fb96326c543eab8c0f689444be937d71f0
Umar Bashir Rather <umarbashir93@outlook.com>
2021-03-28 22:22:34 +0530

    Error Handing Added

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const alert = document.querySelector('.alert');
 document.querySelector('#booking-keeping-form').addEventListener('submit', addBook);
 document.querySelector('.item-list').addEventListener('click', deleteRecord);
 
@@ -6,19 +7,23 @@ function addBook(e) {
     const bookName = document.querySelector('#name').value;
     const isbn = document.querySelector('#isbn').value;
     const listItem = document.querySelector('.item-list');
-
-    // Creating New item
-    const newItem = document.createElement('tr');
-    newItem.className = 'item';
-    newItem.innerHTML = `
-    <td>${listItem.children.length + 1}</td>
-    <td>${author}</td>
-    <td>${bookName}</td>
-    <td>${isbn}</td>
-    <td class="text-center"><a href="#" class="btn btn-danger py-1 pxs-3 delete"><i class="fas fa-times"></i></a></td>
-    `
-    // Adding New Created List to List Item
-    listItem.appendChild(newItem);
+    if (author === '' || bookName === '' || isbn === '') {
+        FailedAlert();
+    } else {
+        // Creating New item
+        const newItem = document.createElement('tr');
+        newItem.className = 'item';
+        newItem.innerHTML = `
+        <td>${listItem.children.length + 1}</td>
+        <td>${author}</td>
+        <td>${bookName}</td>
+        <td>${isbn}</td>
+        <td class="text-center"><a href="#" class="btn btn-danger py-1 pxs-3 delete"><i class="fas fa-times"></i></a></td>
+        `
+        // Adding New Created List to List Item
+        listItem.appendChild(newItem);
+        successAlert();
+    }
     
     // Reset Form Input Fields
     resetForm();
@@ -41,4 +46,16 @@ function deleteRecord(e) {
     } 
     // Prevent Default
     e.preventDefault();
+}
+
+function FailedAlert() {
+    const failedAlert = document.querySelector('.alert-danger')
+    failedAlert.classList.add('active');
+    setTimeout(() => failedAlert.classList.remove('active'), 3000)
+}
+
+function successAlert() {
+    const successAlert = document.querySelector('.alert-success')
+    successAlert.classList.add('active');
+    setTimeout(() => successAlert.classList.remove('active'), 3000)
 }
